61935e9f272355f2bcd3a10223f359472464855f
Dmnitry Kartashov <.email@.com>
2025-12-26 23:33:28 +0300

    Добавил возведение в степень и квадратный корень

--- operations.py
+++ operations.py
@@ -13,3 +13,11 @@ def divide(a, b):
 def mod(a, b):
     if b==0: return 'Ошибка'
     return a % b
+import math
+
+def power(a, b):
+    return a ** b
+
+def sqrt_num(a):
+    if a<0: return 'Ошибка'
+    return math.sqrt(a)
