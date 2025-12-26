7ef3ccb44df5b82d4d90e0fc4353c497b9a07ff9
Dmnitry Kartashov <.email@.com>
2025-12-26 23:34:11 +0300

    Связал GUI с математическими функциями

--- gui.py
+++ gui.py
@@ -1,1 +1,8 @@
+def button_click(value):
+    pass
 
+def button_clear():
+    pass
+
+def button_equals():
+    pass
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from operations import *
+from memory import *
 
+def main():
+    print('Калькулятор запущен')
+
+if __name__=='__main__':
+    main()
